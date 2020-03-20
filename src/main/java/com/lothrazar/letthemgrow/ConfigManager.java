9ef04de9bc450015bbf11c0113e8b7c904217407
lothrazar <samson.bassett@gmail.com>
2020-03-19 19:01:47 -0700

    refactor growth event

--- src/main/java/com/lothrazar/letthemgrow/ConfigManager.java
+++ src/main/java/com/lothrazar/letthemgrow/ConfigManager.java
@@ -4,20 +4,20 @@ import java.nio.file.Path;
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
 import com.electronwill.nightconfig.core.io.WritingMode;
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
+import net.minecraftforge.common.ForgeConfigSpec.IntValue;
 
 public class ConfigManager {
 
   private static final ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
   private static ForgeConfigSpec COMMON_CONFIG;
-  private static BooleanValue TOOLTIPS;
+  private static IntValue CHANCE;
   static {
     initConfig();
   }
 
   private static void initConfig() {
-    COMMON_BUILDER.comment("General settings").push(ExampleMod.MODID);
-    TOOLTIPS = COMMON_BUILDER.comment("Testing config Tooltip").define("itemTooltip", true);
+    COMMON_BUILDER.comment("General settings").push(AnimalGrowthMod.MODID);
+    CHANCE = COMMON_BUILDER.comment("Once every tick, this is the percentage chance that growth will be blocked; so 99 is very slow, and 1 is fast.").defineInRange("slowdownChance", 50, 1, 99);
     COMMON_BUILDER.pop();
     COMMON_CONFIG = COMMON_BUILDER.build();
   }
@@ -32,7 +32,7 @@ public class ConfigManager {
     COMMON_CONFIG.setConfig(configData);
   }
 
-  public boolean tooltips() {
-    return TOOLTIPS.get();
+  public int chanceBlock() {
+    return CHANCE.get();
   }
 }
---
+++ src/main/java/com/lothrazar/letthemgrow/GrowthEvents.java
@@ -0,0 +1,62 @@
+package com.lothrazar.letthemgrow;
+
+import net.minecraft.entity.AgeableEntity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.EntityInteract;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class GrowthEvents {
+
+  @SubscribeEvent
+  public void onLivingUpdateEvent(LivingUpdateEvent event) {
+    World world = event.getEntity().world;
+    if (!world.isRemote
+        && event.getEntityLiving() instanceof AnimalEntity) {
+      AnimalEntity child = (AnimalEntity) event.getEntityLiving();
+      // int NEWBABY = -24000;
+      int FULLGROWN = 0;
+      if (child.getGrowingAge() < FULLGROWN) {
+        //it has a 50% chance of not growing
+        if (world.rand.nextDouble() * 100 < AnimalGrowthMod.config.chanceBlock()) {
+          AnimalGrowthMod.LOGGER.info("slow down");
+          child.setGrowingAge(child.getGrowingAge() - 1);
+        }
+      }
+      /**
+       * The age value may be negative or positive or zero. If it's negative, it get's incremented on each tick, if it's positive, it get's decremented each tick. Don't confuse this with
+       * EntityLiving.getAge. With a negative value the Entity is considered a child.
+       */
+      //ageable entity LivingTick does this
+      //      if (this.isAlive()) {
+      //        int i = this.getGrowingAge();
+      //        if (i < 0) {
+      //           ++i;
+      //           this.setGrowingAge(i);
+      //        } else if (i > 0) {
+      //           --i;
+      //           this.setGrowingAge(i);
+      //        } }
+    }
+  }
+
+  @SubscribeEvent
+  public void onServerStarting(EntityInteract event) {
+    if (event.getTarget() instanceof AgeableEntity) {
+      AgeableEntity growing = (AgeableEntity) event.getTarget();
+      if (growing.isChild()) {
+        if (growing instanceof AnimalEntity) {
+          //one subclass down from ageable
+          //
+          //
+          AnimalEntity child = (AnimalEntity) growing;
+          if (child.isBreedingItem(event.getItemStack())) {
+            AnimalGrowthMod.LOGGER.info("dont feed eh");
+            event.setCanceled(true);
+          }
+        }
+      }
+    }
+  }
+}
