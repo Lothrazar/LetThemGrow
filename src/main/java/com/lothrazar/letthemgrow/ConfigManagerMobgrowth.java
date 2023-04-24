95d811f5301012f6e78efcbd3b3fd083b9895d2b
lothrazar <samson.bassett@gmail.com>
2023-04-24 06:20:57 -0700

    1.19.4 porting

---
+++ src/main/java/com/lothrazar/letthemgrow/ConfigManagerMobgrowth.java
@@ -0,0 +1,55 @@
+package com.lothrazar.letthemgrow;
+
+import com.lothrazar.library.config.ConfigTemplate;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
+import net.minecraftforge.common.ForgeConfigSpec.IntValue;
+
+public class ConfigManagerMobgrowth extends ConfigTemplate {
+
+  private static final ForgeConfigSpec CONFIG;
+  private static IntValue ANIMALCHANCE;
+  private static BooleanValue DISABLEFEEDING;
+  private static BooleanValue MILKNERF;
+  private static IntValue CROPSCHANCE;
+  private static IntValue SAPLINGCHANCE;
+  static {
+    final ForgeConfigSpec.Builder BUILDER = builder();
+    BUILDER.comment("General settings").push(GrowthMod.MODID);
+    MILKNERF = BUILDER.comment("When a cow is milked too often it becomes un-milkable for a time")
+        .define("limitedMilk", true);
+    SAPLINGCHANCE = BUILDER.comment("Once every tick, this is the percentage chance that growth will be blocked; so 99 is very slow, and 1 is fast.")
+        .defineInRange("saplingSlowdownChance", 75, 0, 100);
+    CROPSCHANCE = BUILDER.comment("Once every tick, this is the percentage chance that growth will be blocked; so 99 is very slow, and 1 is fast.")
+        .defineInRange("cropsSlowdownChance", 50, 0, 100);
+    ANIMALCHANCE = BUILDER.comment("Once every tick, this is the percentage chance that growth will be blocked; so 99 is very slow, and 1 is fast.")
+        .defineInRange("livestockSlowdownChance", 70, 0, 100);
+    DISABLEFEEDING = BUILDER.comment("True to disable feeding any compatible items to child animals").define("blockFeedingYoung", true);
+    BUILDER.pop();
+    CONFIG = BUILDER.build();
+  }
+
+  public ConfigManagerMobgrowth() {
+    CONFIG.setConfig(setup(GrowthMod.MODID));
+  }
+
+  public int getCropsChance() {
+    return CROPSCHANCE.get();
+  }
+
+  public int getAnimalChance() {
+    return ANIMALCHANCE.get();
+  }
+
+  public int getSaplingChance() {
+    return SAPLINGCHANCE.get();
+  }
+
+  public boolean disableFeeding() {
+    return DISABLEFEEDING.get();
+  }
+
+  public boolean milkNerf() {
+    return MILKNERF.get();
+  }
+}
--- src/main/java/com/lothrazar/letthemgrow/GrowthMod.java
+++ src/main/java/com/lothrazar/letthemgrow/GrowthMod.java
@@ -1,20 +1,19 @@
 package com.lothrazar.letthemgrow;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod(GrowthMod.MODID)
 public class GrowthMod {
 
   public static final String MODID = "letthemgrow";
   public static final Logger LOGGER = LogManager.getLogger();
-  public static ConfigManager CONFIG;
+  public static ConfigManagerMobgrowth CONFIG;
 
   public GrowthMod() {
+    new ConfigManagerMobgrowth();
     MinecraftForge.EVENT_BUS.register(new GrowthEvents());
-    CONFIG = new ConfigManager(FMLPaths.CONFIGDIR.get().resolve(MODID + ".toml"));
   }
 }
