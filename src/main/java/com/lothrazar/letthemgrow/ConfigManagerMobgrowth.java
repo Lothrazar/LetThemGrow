792d4150d9e06440f7de069bf5390e2e17685d44
Sam <lothrazar@hotmail.com>
2026-07-18 13:51:05 -0700

    finishing off 1.21 port

--- src/main/java/com/lothrazar/letthemgrow/ConfigManagerMobgrowth.java
+++ src/main/java/com/lothrazar/letthemgrow/ConfigManagerMobgrowth.java
@@ -15,14 +15,14 @@ public class ConfigManagerMobgrowth {
   static {
     final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
     BUILDER.comment("General settings").push(LetThemGrowMod.MODID);
-    MILKNERF = BUILDER.comment("When a cow is milked too often it becomes un-milkable for a time")
-        .define("limitedMilk", true);
+    MILKNERF = BUILDER.comment("If true, then when a cow is milked too often it becomes un-milkable for a time")
+        .define("limitedMilk", false);
     SAPLINGCHANCE = BUILDER.comment("Once every tick, this is the percentage chance that growth will be blocked; so 99 is very slow, and 1 is fast.")
-        .defineInRange("saplingSlowdownChance", 75, 0, 100);
+        .defineInRange("saplingSlowdownChance", 0, 0, 100);
     CROPSCHANCE = BUILDER.comment("Once every tick, this is the percentage chance that growth will be blocked; so 99 is very slow, and 1 is fast.")
-        .defineInRange("cropsSlowdownChance", 50, 0, 100);
+        .defineInRange("cropsSlowdownChance", 0, 0, 100);
     ANIMALCHANCE = BUILDER.comment("Once every tick, this is the percentage chance that growth will be blocked; so 99 is very slow, and 1 is fast.")
-        .defineInRange("livestockSlowdownChance", 70, 0, 100);
+        .defineInRange("livestockSlowdownChance", 0, 0, 100);
     DISABLEFEEDING = BUILDER.comment("True to disable feeding any compatible items to child animals").define("blockFeedingYoung", true);
     BUILDER.pop();
     CONFIG = BUILDER.build();
